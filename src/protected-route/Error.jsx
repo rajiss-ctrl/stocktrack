15bcd1b3b382b0ae40c81d2e32a000787dfa1cae
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2023-04-10 06:57:36 +0100

    setTimeout to return back to homepage set in error page

--- src/protected-route/Error.jsx
+++ src/protected-route/Error.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Err from "../assets/images/error.png";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Error = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  }, []);
   return (
     <div className="h-[100vh] text-[red] flex justify-center flex-col items-center">
       <img src={Err} alt="404 error" />
+      <Link to="/">Go back</Link>
     </div>
   );
 };
--- src/protected-route/ProtectedRoute.jsx
+++ src/protected-route/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const ProtectedRoute = () => {
   const user = useSelector((state) => state.user.user);
