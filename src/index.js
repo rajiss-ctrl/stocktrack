fa8af5a795eb85d5265699d2e5b715d3cd972691
rajiss-ctrl <omosanjos77#gmail.com>
2024-04-06 12:29:27 +0100

    deploying

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === "production") {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
       <Provider store={store}>
         <Routes>
           <Route path={"/*"} element={<App />} />
