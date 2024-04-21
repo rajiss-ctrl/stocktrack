5bfa90c2ef902cae88f8a0bf201eaf7413972343
rajiss-ctrl <omosanjos77#gmail.com>
2024-04-21 19:19:57 +0100

    recent adjustment

--- src/pages/BusinessProfile.jsx
+++ src/pages/BusinessProfile.jsx
@@ -40,6 +40,7 @@ const BusinessProfile = () => {
   });
 
   const handleFileReader = (e) => {
+    e.preventDefault()
     const reader = new FileReader();
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
@@ -82,16 +83,18 @@ const BusinessProfile = () => {
 
   return (
     <div className="relative min-h-screen bg-[#F3F5F7]">
-      <Link className='absolute hover:shadow-lg top-6 sm:top-6 text-gray-600 shadow-xl bg-transparent text-xs px-3 py-1 rounded-sm sm:rounded-md left-1/2 transform -translate-x-1/2 -translate-y-1/2' to='/dashboard'>Dashboard</Link>
+      <Link className='absolute hover:shadow-lg top-6 sm:top-6 font-bold border border-slate-700 text-gray-600 shadow-xl bg-transparent text-xs px-3 py-1 rounded-sm sm:rounded-md left-1/2 transform -translate-x-1/2 -translate-y-1/2' to='/dashboard'>Dashboard</Link>
     
-    <div className="flex flex-col sm:justify-between sm:flex-row items-center  px-2 sm:px-0 mt-24 sm:mt-0 ">
-      <div className="px-2 sm:px-14 sm:w-1/2">
-      <div className=" w-full max-h-screen flex flex-col justify-center items-center  sm:shadow bg-[#eceff1] rounded p-3">
-        <h3 className="text-center font-bold text-sm text-gray-400 mb-6">
+    <div className={`md:px-12 flex flex-col lg:justify-between lg:flex-row items-center  lg:px-0 ${userData.lenght === 0 ? 'mt-24' :  'mt-8' } md:mt-0`}>
+      {userData.lenght === 0 ?
+   
+      <div className="px-0 md:mb-24 lg:mb-0 md:w-full  lg:px-14 lg:w-1/2">
+      <div className="mb-10 md:px-12 lg:px-0 sm:mb-0 w-full max-h-screen flex flex-col justify-center items-center  sm:shadow sm:bg-[#eceff1] rounded ">
+        <h3 className="text-center mt-5 font-bold text-sm text-gray-400 mb-6">
           Create Business Profile
         </h3>
 
-      <form className="w-full flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+      <form className="w-full flex flex-col py-2 px-5" onSubmit={handleSubmit(onSubmit)}>
         {/* The handleSubmit function sends the form details to Firestore */}
         <input
           type="text"
@@ -131,7 +134,7 @@ const BusinessProfile = () => {
           </span>
         )}
         <textarea
-          className=" outline-yellow-50 px-4 w-full mb-6 border capitalize rounded"
+          className=" outline-yellow-50 px-4 w-full mb-4 border capitalize rounded"
           id="about"
           placeholder="About The Business"
           {...register("about")}
@@ -143,8 +146,8 @@ const BusinessProfile = () => {
         )}
 
         <div className="flex items-center space-x-4 w-full">
-          <div className="flex flex-col w-1/2 pl-16">
-            <img src={logo} alt="Logo" className="w-9 h-9 sm:w-14 sm:h-14 rounded-[50%]"  />
+          <div className="flex flex-col  w-1/2 mb-2 pl-16">
+            <img src={logo} alt="Logo" className="shadow-lg w-9 h-9 sm:w-14 sm:h-14 rounded-[50%]"  />
           </div>
           <div className="w-full">
           <label
@@ -183,12 +186,38 @@ const BusinessProfile = () => {
       </form>
       </div>
       </div> 
-      {/* profile */}
-      <div className="bg-white sm:w-1/2 sm:h-screen">
+        :
+        <div className="px-0 md:mb-24 lg:mb-0 md:w-full  lg:px-5 lg:w-1/2 ">
         {
+        userData.map((user)=>{
+          return(
+            <div key={user.id} className="flex  bg-transparent w-full pb-5  justify-center items-center flex-col">
+           
+              <div className="text-sm pt-5 flex justify-center items-center flex-col">
+              
+                <article className="pt-5">
+                  <h4 className="leading-normal md:leading-relaxed font-bold text-xl lg:leading-loose text-center pb-2">Our Mission</h4>
+                  <p className="leading-normal  md:leading-relaxed lg:leading-loose sm:mx-4 text-black text-sm px-4 py-2">{user.about}</p>
+                </article>
+            </div>
+            </div>
+          )
+        })
+      }
+      </div>
+      }
+      {/* profile */}
+      <div className="bg-white   lg:mb-0  min-h-[300px]  lg:w-1/2 lg:h-screen">
+      {userData.length === 0 ?
+      <div className="mt-16 sm:mt-0 flex justify-center items-center h-full">
+        <h2>BUSINESS PROFILE INFO</h2>
+      </div>
+        :
+        
           userData.map((user)=>{
             return(
               <div key={user.id} className="flex py-16 bg-white w-full   justify-center items-center flex-col">
+               
                 <div className="flex flex-col justify-center items-center"> 
                   <img src={user.logo} alt="Logo" className=" w-14 h-14 rounded-[50%]"  />
                   <h1 className="pt-1  text-sm">{user.businessName}</h1>
@@ -201,6 +230,7 @@ const BusinessProfile = () => {
                     <p className="leading-normal md:leading-relaxed lg:leading-loose sm:mx-4 text-gray-500 sm:text-black text-sm sm:text-xs px-4 py-2">{user.about}</p>
                   </article>
               </div>
+
               </div>
             )
           })
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,4 @@
-// import React, { useEffect } from "react";
-
-// import NavBar from "../components/NavBar";
-
-// import HeroSection from "./pages-components/HeroSection";
-// import HomeForms from "./pages-components/HomeForms";
-// function Home() {
-//   return (
-//     <div className=" bg-gradient-to-r from-dark-purple to-green-800 bg-clip  overflow-hidden">
-//       <NavBar />
-//       <div className=" w-full relative">
-//         <HeroSection />
-//         {/* form */}
-//         <HomeForms />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-import React, { useEffect } from "react";
+import React, { useEffect,useState,  } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footers/Footer";
 import Navbar from "../components/Navbars/IndexNavbar";
@@ -30,23 +9,48 @@ import Drownfan from "../assets/img/drownfan.png";
 import Parcels from "../assets/img/parcels.png";
 import StockTrack from "../assets/img/stocktrack-ill.png";
 import Dashboard from "../assets/video/dashboard-illustration.mp4";
+import { useSpring, animated } from 'react-spring';
+import Lottie from 'lottie-react';
+// import animationData from '../assets/Animation-box.json'
+import animationData from '../assets/Animation-note.json'
 
  function Home() {
   const navigate = useNavigate()
+  const [loaded, setLoaded] = useState(false);
+
+  // Animation for revealing the image slowly on first load
+  const imageAnimation = useSpring({
+    opacity: loaded ? 1 : 0,
+    from: { opacity: 0 },
+  });
+
+  // Set loaded to true after a short delay to trigger the animation
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     document.title = "StocTrack Home page"; // Set your desired page title here
   }, []); // This effect runs only once after the component mounts
   
+
+
   return (
     <>
       <Navbar fixed />
-      <section className=" relative  bg-[#F6F8F9] pt-16 pb-5 md:pb-24 lg:pb-0 md:pt-28 lg:pt-20 xl:pt-14 h-auto overflow-hidden">
+    
+      <section className=" relative z-40 bg-[#F6F8F9] pt-16 pb-5 md:pb-24 lg:pb-0 md:pt-28 lg:pt-20 xl:pt-14 h-auto overflow-hidden">
 
-      <div className="hidden lg:block lg:absolute z-30 lg:right-[38%] xl:right-[36%] lg:top-36">
+      {/* <div className="hidden lg:block lg:absolute z-30 lg:right-[38%] xl:right-[36%] lg:top-36">
         <img src={HeroDrown} className="rotate-animation" alt="...."/>
+      </div> */}
+      <div className="hidden lg:block w-40 absolute lg:right-[25%]  lg:top-36 z-50">
+        <Lottie animationData={animationData}/>
       </div>
-
         <div className="custom-bg w-full flex items-center justify-center flex-col lg:flex-row lg:justify-between">
             <div className="w-full xl:pl-20 sm:pt-0">
               <h2 className="tracking-tight md:tracking-wide lg:tracking-widest px-4 sm:px-0 pt-10 sm:pt-0 lg:pl-16 xl:pl-8  xl:pr-10 text-center lg:text-left text-[#46148B] font-bold text-[1.6rem] leading-[2rem]  sm:text-3xl md:text-6xl lg:text-4xl xl:text-5xl ">
@@ -66,9 +70,11 @@ import Dashboard from "../assets/video/dashboard-illustration.mp4";
                 className='lg:hidden md:w-[40rem] md:mt-10 mt-5'
                 src={HeroMobile} alt="..." />
               </div>
-            <img
+            <animated.img
                 className="hidden lg:block xl:w-[70%] xl:pt-10 absolute -right-4 sm:-right-10 lg:-bottom-10 sm:bottom-0 lg:static rotate-12 lg:rotate-0"
                 src={Hero}
+                style={imageAnimation}
+                onLoad={() => setLoaded(true)}
                 alt="..."
               />
       
@@ -77,6 +83,7 @@ import Dashboard from "../assets/video/dashboard-illustration.mp4";
             <img src={HeroDrown} alt="...."/>
           </div>
       </section>
+      
       <section className="w-full text-sm sm:text-xl text-[#7b7b7b]  md:py-24 py-14 flex flex-col justify-center items-center bg-white relative z-10">
           <h2 className="font-bold tracking-tight md:tracking-wide lg:tracking-widest text-center text-lg sm:2xl pb-5 px-6 md:px-44">Elevate Efficiency: Redefine <span className="text-[#86E372] font-bold">Inventory Management</span> Now!</h2>
           <div className="relative flex flex-col justify-center items-center">
@@ -102,7 +109,7 @@ import Dashboard from "../assets/video/dashboard-illustration.mp4";
           <img src={StockTrack} className="w-full" alt="laptop and mobile" />
         </div>
         <div className="w-full md:w-[50%] ">
-         <h3 className="tracking-tight md:tracking-wide lg:tracking-widest text-lg pt-7 sm:pt-0 lg:text-xl  font-bold  text-[#7b7b7b]">StockTrack: Empowering Your Inventory Management Effortlessly</h3>
+         <h3 className="tracking-tight md:tracking-wide lg:tracking-widest text-lg pt-7 sm:pt-0 lg:text-xl  font-bold  text-[#252525]">StockTrack: Empowering Your Inventory Management Effortlessly</h3>
           <p className="tracking-tight md:tracking-wide lg:tracking-widest text-sm pt-2  md:text-base">Revolutionize your inventory control with our state-of-the-art Dashboard. Seamlessly manage stock, track movements, and make informed decisions. A user-friendly interface following the best industrial practices for unparalleled efficiency and success.</p>
         </div>
       </section>
