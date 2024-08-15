5fdb19a059f42d8eff074db78db3c1715ae104c2
HackyRaji <hackyraji02@gmail.com>
2024-08-15 21:09:16 +0100

    adjustment in padding

--- src/pages/pages-components/DashboardDescription.jsx
+++ src/pages/pages-components/DashboardDescription.jsx
@@ -8,7 +8,7 @@ import animationData from '../../assets/Animation-note.json'
 
 const DashboardDescription = () => {
   return (
-    <section className="w-full text-sm sm:text-xl text-[#7b7b7b]  md:py-40 py-14 flex flex-col justify-center items-center bg-white relative z-10">
+    <section className="w-full text-sm sm:text-xl text-[#7b7b7b]  md:py-40 py-20 flex flex-col justify-center items-center bg-white relative z-10">
     <h2 className="font-extrabold tracking-tight md:tracking-wide lg:tracking-widest text-center text-lg sm:2xl pb-5 px-6 md:px-44">Elevate Efficiency: Redefine <span className="text-[#86E372] font-bold">Inventory Management</span> Now!</h2>
     <div className="relative flex flex-col justify-center items-center">
       {/* <img className="absolute z-10 left-6 md:left-16 -bottom-5 md:-bottom-16 w-[25%]" src={Parcels} alt="..." /> */}
